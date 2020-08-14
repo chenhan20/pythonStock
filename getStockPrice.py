b5ad29c00c43df7759ff73852611eed63bf6c295
SteveChuang <steve.chuang@careline.com.tw>
2020-08-14 17:51:45 +0800

    commit

--- getStockPrice.py
+++ getStockPrice.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import json
 import requests 
+import datetime
+
+now = datetime.datetime.now()
+
 stock_no = input('輸入你想查找股票關鍵字或代號: ')
 
-url = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?date=20200814&stockNo='+ stock_no
+url = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?date=' + now.strftime("%Y%m%d") +'&stockNo='+ stock_no
 # 偽瀏覽器
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
