38280f4ab0fa9c5f256ddf03f113af7f5e98168a
SteveChuang <steve.chuang@careline.com.tw>
2020-08-17 17:43:06 +0800

    三大法人

--- getStockThree.py
+++ getStockThree.py
@@ -5,21 +5,27 @@ import datetime
 
 now = datetime.datetime.now()
 
-stock_no = input('輸入你想查找股票關鍵字或代號: ')
+# stock_no = input('輸入你想查找股票關鍵字或代號: ')
+url = 'https://www.twse.com.tw/fund/BFI82U?response=json&dayDate=' + now.strftime("%Y%m%d") + '&type=day'
 
-url = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?date=' + now.strftime("%Y%m%d") +'&stockNo='+ stock_no
 # 偽瀏覽器
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
+def converter(data):
+    for i in range(len(data)):
+        for d in range(len(data[i])):
+            converterNumber = data[i][d].replace(',','')
+            if converterNumber.isdigit():
+                converterNumber = int(converterNumber)
+            data[i][d] = converterNumber
 
-res  = requests.get(url, headers=headers)
 
-# df = pd.DataFrame(res.text)
-# df
+res = requests.get(url, headers=headers)
+
 stockData=json.loads(res.text)
 
-# print(stockData['stat'])
 if stockData['stat']=='OK':
+    converter(stockData['data'])
     df = pd.DataFrame(stockData['data'],columns=stockData['fields'])
     pd.set_option('display.unicode.ambiguous_as_wide', True)
     pd.set_option('display.unicode.east_asian_width', True)
